da00379d297b0ec3c8d292ecc26d3f1484cfb946
ismatullayevs <ismatullayevpro@gmail.com>
2021-12-14 17:01:25 +0500

    ch12

--- blogger/settings.py
+++ blogger/settings.py
@@ -19,7 +19,7 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ["ismatullayev-blog.herokuapp.com"]
+ALLOWED_HOSTS = ["https://ismatullayev-blog.herokuapp.com"]
 
 
 # Application definition
