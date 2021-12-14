bd6974dbf2be00ec1d92f0019f33e8b84684400c
ismatullayevs <ismatullayevpro@gmail.com>
2021-12-14 18:34:47 +0500

    heroku

--- blogger/settings.py
+++ blogger/settings.py
@@ -9,13 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "4db3ptp4scikgmss6vp52yzku!-^!(i&%wh2kmfc^dk64=bp0t"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ismatullayev-blog.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
